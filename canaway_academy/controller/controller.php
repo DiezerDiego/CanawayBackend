f56a5e5e599199178a7fc370574b99f2c05c3d4b
Diego Cayllahua <diezer_diego@hotmail.com>
2023-03-13 10:52:37 -0500

    add an additional field 'extra_column'

--- canaway_academy/controller/controller.php
+++ canaway_academy/controller/controller.php
@@ -4,7 +4,7 @@ try {
     $inputJSON = file_get_contents('php://input');
     $input = json_decode($inputJSON, TRUE); //convert JSON into array
 
-    if(empty( $input["name"]) || empty($input["email"]) || empty($input["english_level"])){
+    if(empty( $input["name"]) || empty($input["email"]) || empty($input["english_level"]) || empty($input["extra_column"])){
       echo json_encode(["data"=>["note"=>"You can send next fields: name, email, english_level"]]);
       http_response_code(505);
       throw new Exception("You can send next fields: name, email, english_level");
@@ -13,7 +13,8 @@ try {
     $name = $input["name"];
     $email = $input["email"];
     $english_level = $input["english_level"];
-    $user_service = new UserService($name, $email, $english_level);
+    $extra_column=$input["extra_column"];
+    $user_service = new UserService($name, $email, $english_level,$extra_column);
     $user_search = $user_service->getUser();
     if (!$user_search) {
         $user_service->createUser();
