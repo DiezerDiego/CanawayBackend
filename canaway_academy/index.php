54f5da0501e7f3b1f63055291fe923bb002faffc
Diego Cayllahua <diezer_diego@hotmail.com>
2023-03-13 10:49:33 -0500

    IF THE REQUEST METHOD IS OPTIONS , RETURN EMPTY WITH THE HEADERS CORS

--- canaway_academy/index.php
+++ canaway_academy/index.php
@@ -1,21 +1,19 @@
 <?php
-$tokenServer='$213c/*sScxS5ax:sad_s';
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-  echo  json_encode(['data'=>
-  ["message"=>"Only request is permitted: Post","note"=>"You can send next fields: name, email, english_level"]]);
-        throw new Exception("You can send next fields: name, email, english_level");
 
-}else{
+$tokenServer='$213c/*sScxS5ax:sad_s';
+
   $headers = apache_request_headers();
   $token = explode("Bearer ",$headers['Authorization'])[1];
-  if($token==$tokenServer){
-    require_once(__DIR__."/controller/controller.php");
-  }else{
+  if($_SERVER["REQUEST_METHOD"]!="OPTIONS"){
+    if($token==$tokenServer){
+      require_once(__DIR__."/controller/controller.php");
+    }else{
 
-    echo json_encode(["data"=>["message"=>"unauthorized, token is incorrect or empty"]]);
-    http_response_code(401);
-    throw new Exception("unauthorized, token is incorrect or empty");
+      echo json_encode(["data"=>["message"=>"unauthorized, token is incorrect or empty"]]);
+      http_response_code(401);
+      throw new Exception("unauthorized, token is incorrect or empty");
+
+    }
 
   }
 
-}
